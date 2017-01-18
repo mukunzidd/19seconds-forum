441393c723df305a0d8673a3f89cdca171e2be8d
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-01-18 14:46:22 +0200

    Added relationships between comments posts and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117155643) do
+ActiveRecord::Schema.define(version: 20170118123006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170117155643) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170117155643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "comment_id"
   end
 
   create_table "users", force: :cascade do |t|
